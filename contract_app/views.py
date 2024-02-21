8be17c4f2531a66223e2e2327610b5f0937f3bfb
Meron <merronmuche@gmail.com>
2024-02-21 22:59:09 +0300

    add html code to change UI

--- contract_app/views.py
+++ contract_app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import HttpResponse, render
 from .utils.embed_text import embed_text
 from .utils.similarity import cosine_similarity
 from dotenv import load_dotenv
-from .models import TextChunk
+from .models import Document, TextChunk
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
 
@@ -19,7 +19,7 @@ def generate_response_with_gpt_turbo(user_question, relevant_text_chunk):
     # Combine the user's question and relevant text chunk into a prompt
     prompt = f"based on the thi relevant_text_chunk, give an answer to \
               the users question. restrict yourself to the given data only. \
-              if you can't get an answer based on the data, you can feel free to \
+              NOTE: if you can't get an answer based on the data, you have to \
               say i don't know.\n + {user_question}\n{relevant_text_chunk}\n"
 
     # Implement the GPT-3.5 Turbo API call to generate a response
@@ -30,7 +30,6 @@ def generate_response_with_gpt_turbo(user_question, relevant_text_chunk):
             ),
             
         ])
-    
 
     # Assuming response is the generated answer
     return response
@@ -39,6 +38,8 @@ def generate_response(request):
 
     if request.method == 'POST':
         user_question = request.POST.get('input_text')
+        selected_document_name = request.POST.get('document', '')
+        selected_document = Document.objects.filter(pdf_file__icontains=selected_document_name)[0]
 
         # Embed the user's input
         embeded_question = embed_text([user_question])[0]
@@ -47,8 +48,8 @@ def generate_response(request):
         best_text_chunk = None
 
         # Compare with embeddings in TextChunk objects
-        chunks = TextChunk.objects.filter()
-        for text_chunk in TextChunk.objects.all():
+        chunks = TextChunk.objects.filter(document=selected_document)
+        for text_chunk in chunks:
             similarity = cosine_similarity(embeded_question, text_chunk.embed)
 
             if similarity > highest_similarity:
@@ -66,4 +67,6 @@ def generate_response(request):
             return HttpResponse("No similar documents found.")
     
     elif request.method == 'GET':
-        return render(request, 'contract_app/generate_response.html')
+
+        documents = Document.objects.all()
+        return render(request, 'contract_app/generate_response.html', context={'documents': documents})
