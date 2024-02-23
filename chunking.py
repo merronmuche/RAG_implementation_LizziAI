297138d2134799ca16578c03e5c2d5a5af4f3a7e
Meron <merronmuche@gmail.com>
2024-02-23 12:24:29 +0300

    generate response code

--- chunking.py
+++ chunking.py
@@ -6,31 +6,54 @@ from langchain_openai import OpenAIEmbeddings
 from dotenv import load_dotenv
 from chunk_document import extract_text_from_pdf
 
-# Load your PDF document
-path = 'datalizzy/Raptor Contract.docx'
-path1 = 'datalizzy/Robinson Advisory.docx'
-docs1 = extract_text_from_pdf(path)
-docs2 = extract_text_from_pdf(path1)
-docs = docs1 + docs2
+from langchain.document_loaders import PyMuPDFLoader, LarkSuiteDocLoader, UnstructuredWordDocumentLoader
 
-# Use the parent splitter to split the documents
+# path = '/home/meron/tutorial/Advanced-RAG/Raptor_Contract.pdf'
+path = '/home/meron/Documents/work/tenacademy/week11/RAG_implementation_LizziAI/datalizzy/Raptor Contract.docx'
+path1 = '/home/meron/Documents/work/tenacademy/week11/RAG_implementation_LizziAI/datalizzy/Robinson Advisory.docx'
+loader = UnstructuredWordDocumentLoader(path)
+doc1= loader.load()
+loader = UnstructuredWordDocumentLoader(path1)
+doc2= loader.load()
+
+docs = doc1 + doc2
+text = docs[5].page_content
+print(text)
+
+
+
+
+from langchain.storage import InMemoryStore
+from langchain.retrievers import ParentDocumentRetriever
+
+child_splitter = RecursiveCharacterTextSplitter(chunk_size=120, chunk_overlap=20)
 parent_splitter = RecursiveCharacterTextSplitter(chunk_size=400, chunk_overlap=20)
-split_docs = parent_splitter.split_documents(docs)
-
-# Initialize the embeddings from langchain_openai
-embedding = OpenAIEmbeddings(chunk_size=1)
-
-# Initialize the vectorstore and store
-vectorstore = Chroma(collection_name="full_documents", embedding_function=embedding)
+vectorstore = Chroma(
+    collection_name="full_documents", embedding_function=embedding
+)
 store = InMemoryStore()
-
-# Initialize the retriever with parent and child chunking
 retriever = ParentDocumentRetriever(
     vectorstore=vectorstore,
     docstore=store,
-    child_splitter=RecursiveCharacterTextSplitter(chunk_size=120, chunk_overlap=20),
+    child_splitter=child_splitter,
     parent_splitter=parent_splitter
 )
-
 # Add the split documents to the retriever
-retriever.add_documents(split_docs, ids=None)
+retriever.add_documents(docs, ids=None)
+vectorstore.similarity_search("How much is the escrow amount?")
+retriever.get_relevant_documents("How much is the escrow amount?")
+from langchain.chat_models import ChatOpenAI
+from langchain.retrievers.multi_query import MultiQueryRetriever
+
+llm = ChatOpenAI(
+        temperature=0,
+        max_tokens=800,
+        model_kwargs={"top_p": 0, "frequency_penalty": 0, "presence_penalty": 0},
+    )
+
+
+retriever = MultiQueryRetriever.from_llm(
+    retriever=vectorstore.as_retriever(), llm=llm
+)
+
+response = llm()
--- contract_app/utils/prompt_gen.py
+++
@@ -1,8 +0,0 @@
-
-
-def get_prompt(input_text, length_of_line):
-
-    return f"Generated Prompt: {input_text[:int(length_of_line)]}..."
-
-
-
