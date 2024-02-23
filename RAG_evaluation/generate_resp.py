9bf6173d6bbdd28f4102d1ceaf1ecb9c5f21e57b
Meron <merronmuche@gmail.com>
2024-02-23 23:41:17 +0300

    get respnose from the django helper function.

---
+++ RAG_evaluation/generate_resp.py
@@ -0,0 +1,49 @@
+
+
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'lizzy_AI.settings')
+django.setup()
+
+import sys
+
+from contract_app.views import generate_response_with_gpt_turbo
+sys.path.append('..')
+
+from RAG.embed_text import embed_text
+from RAG.similarity import cosine_similarity
+from contract_app.models import Document, TextChunk
+
+
+def get_reponse(user_question, selected_document_name):
+    selected_document = Document.objects.filter(pdf_file__icontains=selected_document_name)[0]
+
+    # Embed the user's input
+    embeded_question = embed_text([user_question])[0]
+
+    best_text_chunks = []
+    # Compare with embeddings in TextChunk objects
+    chunks = TextChunk.objects.filter(document=selected_document)
+
+    for text_chunk in chunks:
+        similarity = cosine_similarity(embeded_question, text_chunk.embed)
+        
+        # Add the current chunk to the list if there are less than 5 chunks
+        if len(best_text_chunks) < 5:
+            best_text_chunks.append((similarity, text_chunk.chunk))
+        else:
+            # Check if the current chunk is more similar than the least similar chunk in the list
+            min_similarity_index = min(range(5), key=lambda i: best_text_chunks[i][0])
+            if similarity > best_text_chunks[min_similarity_index][0]:
+                # Replace the least similar chunk with the current chunk
+                best_text_chunks[min_similarity_index] = (similarity, text_chunk.chunk)
+
+    # Extract only the chunks from the tuple
+    best_text_chunks = [chunk for _, chunk in best_text_chunks]
+    total_text = ''.join(best_text_chunks)
+    if best_text_chunks:
+        response = generate_response_with_gpt_turbo(user_question, total_text)
+
+
+    return response.content, total_text
