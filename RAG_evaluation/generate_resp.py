b0e98e408b6d59e4bee5f071ac1ed586b52efffd
Meron <merronmuche@gmail.com>
2024-03-10 20:23:04 +0300

    modifing get_reponse function

--- RAG_evaluation/generate_resp.py
+++ RAG_evaluation/generate_resp.py
@@ -13,21 +13,18 @@ sys.path.append('..')
 
 from RAG.embed_text import embed_text
 from RAG.similarity import cosine_similarity
-from contract_app.models import Document, TextChunk
-from asgiref.sync import sync_to_async
+from contract_app.models import Document, TextChunk, Conversation
 
+def get_reponse(user_question, selected_document_name):
 
-async def get_reponse(user_question, selected_document_name):
+    selected_document = Document.objects.filter(pdf_file=selected_document_name)[0]
 
-    selected_documents = await sync_to_async(Document.objects.filter)(pdf_file__icontains=selected_document_name)
-    selected_document = await sync_to_async(selected_documents.first)()
-
-
-    # Embed the user's input
+    # Embed the user's input   
     embeded_question = embed_text([user_question])[0]
 
     best_text_chunks = []
-    chunks = await sync_to_async(list)(TextChunk.objects.filter(document=selected_document))
+    chunks = TextChunk.objects.filter(document=selected_document)
+    chunks = selected_document.textchunk_set.all()
 
     for text_chunk in chunks:
         similarity = cosine_similarity(embeded_question, text_chunk.embed)
@@ -48,21 +45,21 @@ async def get_reponse(user_question, selected_document_name):
 
     response = None  # Add a default value
     if best_text_chunks:
-        response = generate_response_with_gpt_turbo(user_question, total_text)
+        response = generate_response_with_gpt_turbo(user_question, total_text),
+    
 
-    return response.content if response else None, best_text_chunks
+    return response[0].content 
 
 
 if __name__=="__main__":
 
-    import asyncio
     from RAG_evaluation.read_qa import read_docx
 
     file_path = "/home/meron/Documents/work/tenacademy/week11/RAG_implementation_LizziAI/data/Robinson_Q&A.docx"
     content = read_docx(file_path)
-    selected_document_name = 'docx/Robinson_Advisory_opgMThn.docx'
+    selected_document_name = 'docx'
     
-    response =  asyncio.run(get_reponse('What is your name?', selected_document_name))
+    response =  get_reponse('What is your name?', selected_document_name)
 
     print(response)
 
