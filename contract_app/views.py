7995b1ef9ee0ef5114214783ccd7e8cca88142ef
Meron <merronmuche@gmail.com>
2024-04-19 21:09:48 +0300

    add some  codes in view to pass conversation histrory for llm.

--- contract_app/views.py
+++ contract_app/views.py
@@ -6,15 +6,17 @@ from contract_app.models import Conversation, Document, TextChunk, Topic
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
 from django.contrib.auth.decorators import login_required
+
+
 load_dotenv()
 
 chat = ChatOpenAI(model="gpt-3.5-turbo-1106", temperature=0.2)
 
 
-def generate_response_with_gpt_turbo(user_question, relevant_text_chunk, history=""):
+def generate_response_with_gpt_turbo(user_question, relevant_text_chunk, conversations):
     prompt = (
-        f"AI Language Model Assistant, based on the provided information:\n"
-        f"User Question: '+ \n {history} \n {user_question}'\n"
+        f"AI Language Model Assistant, based on the provided information and conversations:\n"
+        f"User Question: '+ \n {conversations} \n {user_question}'\n"
         f"Relevant Text Chunk: '{relevant_text_chunk}'\n"
         f"Please provide a short, concise and fact-based answer to the user's question:\n"
         f"Answer: "
@@ -43,7 +45,9 @@ def process_question(request, id=None):
     if request.method == "POST":
         user_question = request.POST.get("input_text")
         selected_document_name = request.POST.get("document", "")
-        selected_document = Document.objects.filter(pdf_file__icontains=selected_document_name).first()
+        selected_document = Document.objects.filter(
+            pdf_file__icontains=selected_document_name
+        ).first()
 
         if selected_document:
             embeded_question = embed_text([user_question])[0]
@@ -56,32 +60,60 @@ def process_question(request, id=None):
                 if len(best_text_chunks) < 3:
                     best_text_chunks.append((similarity, text_chunk.chunk))
                 else:
-                    min_similarity_index = min(range(3), key=lambda i: best_text_chunks[i][0])
+                    min_similarity_index = min(
+                        range(3), key=lambda i: best_text_chunks[i][0]
+                    )
                     if similarity > best_text_chunks[min_similarity_index][0]:
-                        best_text_chunks[min_similarity_index] = (similarity, text_chunk.chunk)
+                        best_text_chunks[min_similarity_index] = (
+                            similarity,
+                            text_chunk.chunk,
+                        )
 
             best_text_chunks = [chunk for _, chunk in best_text_chunks]
             total_text = "".join(best_text_chunks)
-            response = generate_response_with_gpt_turbo(user_question, total_text)
+
+            conversation = Conversation.objects.filter(topic_id=id) if id else None
+            history = []
+            if conversation is not None:
+                for conv in conversation:
+                    answer = conv.answer
+                    question = conv.question
+                    add_two = answer + question
+                    history.append(add_two)
+            else:
+                None
+
+            response = generate_response_with_gpt_turbo(
+                user_question, total_text, conversations=history
+            )
 
             if not topic:
                 topic = Topic.objects.create(title=user_question)
-
-            Conversation.objects.create(topic=topic, question=user_question, answer=response.content)
-            conversations = Conversation.objects.filter(topic=topic)
+            conversations = Conversation.objects.create(
+                topic=topic, question=user_question, answer=response.content
+            )
 
             return redirect("topic_view", topic.id if topic else None)
 
-    return render(request, "contract_app/generate_response.html", {
-        "documents": documents,
-        "conversations": conversations,
-        "topics": topics,
-    })
+    documents = Document.objects.all()
+    topics = Topic.objects.all()
+    conversations = Conversation.objects.filter(topic_id=id) if id else None
+
+    return render(
+        request,
+        "contract_app/generate_response.html",
+        {
+            "documents": documents,
+            "conversations": conversations,
+            "topics": topics,
+        },
+    )
 
 
 @login_required
 def generate_response(request):
     return process_question(request)
+
 
 def topic_view(request, id):
     return process_question(request, id)
